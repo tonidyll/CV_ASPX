03ea5770f0f0fde6f68bb7072b2f04069267c318
Antoni Davia <tonidyll@gmail.com>
2022-12-11 20:17:38 +0100

    Projects done

--- Services/RepositoryOfProjects.cs
+++ Services/RepositoryOfProjects.cs
@@ -6,35 +6,35 @@ public class RepositoryOfProjects
     {
         return new List<ClassProjects>() {
             new ClassProjects {
-                Title = "Flowers",
-                Description = "Beauty flowers",
-                Link = "https://amazon.com",
-                ImageURL = "/images/flower.png"
+                Title = "PMemory",
+                Description = "Memory Game",
+                Link = "https://github.com/tonidyll/Pmemory",
+                ImageURL = "/images/pmemory.png"
             },
             new ClassProjects {
-                Title = "Java Script",
-                Description = "Animación realizado en JS",
-                Link = "https://Monlau.com",
+                Title = "RaspberryApacheProject - ONG",
+                Description = "ONG website located into an raspberry apache srv",
+                Link = "https://github.com/tonidyll/RaspberryApacheProject",
+                ImageURL = "/images/webBuild.jpg"
+            }, new ClassProjects {
+                Title = "Driving School",
+                Description = "Created with Java, this project is a platform where to register and manage a driving school",
+                Link = "https://github.com/tonidyll/DrivingSchool",
+                ImageURL = "/images/java.png"
+            }, new ClassProjects {
+                Title = "InteractiveTimeZoneChanger",
+                Description = "This is an interactive map where , clicking into the different enabled zones you can see its actual day and hour",
+                Link = "https://github.com/tonidyll/InteractiveTimeZoneChanger",
                 ImageURL = "/images/js.png"
             }, new ClassProjects {
                 Title = "ASP.net",
-                Description = "WEB realizado en ASP.NET Core",
-                Link = "https://amazon.com",
+                Description = "Desc",
+                Link = "https://desarrolloweb.com/home/asp",
                 ImageURL = "/images/asp.jfif"
             }, new ClassProjects {
-                Title = "Java",
-                Description = "Java Projects",
-                Link = "https://amazon.com",
-                ImageURL = "/images/java.png"
-            }, new ClassProjects {
-                Title = "ASP.net",
-                Description = "Desc",
-                Link = "https://amazon.com",
-                ImageURL = "/images/flower.png"
-            }, new ClassProjects {
                 Title = "React",
-                Description = "WEB realizado en ASP.NET Core",
-                Link = "https://amazon.com",
+                Description = "WEB realizado con framework React",
+                Link = "https://es.reactjs.org/",
                 ImageURL = "/images/react.png"
             },
         };
