151893b1b41ae4c6d11ce83925a0343d9301db76
Antoni Davia Yll <antonidavyll@campus.monlau.com>
2022-12-16 20:30:19 +0100

    closed div tags

--- Models/ClassStudiedCourses.cs
+++ Models/ClassStudiedCourses.cs
@@ -4,11 +4,11 @@ namespace P1_ASP.Models
 {
     public class ClassStudiedCourses
     {
-        public string Title { get; set; }
-        public string ImageURL { get; set; }
-        public string Dates { get; set; }
-        public string Link { get; set; }
-        public string Location { get; set; }
+        public string? Title { get; set; }
+        public string? ImageURL { get; set; }
+        public string? Dates { get; set; }
+        public string? Link { get; set; }
+        public string? Location { get; set; }
 
     }
 }
